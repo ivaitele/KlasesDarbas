8a651f04f38730bb86ebd659b4f2f0b3287a2b6c
Irmantas Vaitele <irmantas.vaitele@gmail.com>
2022-12-12 15:04:44 +0200

    mano pirmas comitas

---
+++ js/mano-skriptas.js
@@ -0,0 +1,21 @@
+// Pirmoji užduotis
+let a = 1;
+console.log('Kintamojo a reikšmė: ' + a);
+a = 2;
+console.log('Kintamojo a reikšmė: ' + a);
+
+//naujas pakeitimas
+
+// Antroji užduotis
+let vardasPavarde = 'Irmantas Vaitele';
+console.log('Mano vardas ir pavardė: ' + vardasPavarde);
+
+
+// Trecioji uzduotis
+const getLastNr = (nr) => parseInt((nr+'').slice(-1))
+
+const count = (metai, menuo, diena) => getLastNr(metai) + getLastNr(menuo) + getLastNr(diena)
+
+const suma = count(1982, 1, 11)
+
+console.log(suma)
