ec6c4e007b2dbaf381c6317c979e9a2af7dc7bd7
Irmantas Vaitele <irmantas.vaitele@gmail.com>
2022-12-13 10:45:23 +0200

    uzduotys javascript

--- js/mano-skriptas.js
+++ js/mano-skriptas.js
@@ -12,6 +12,20 @@ console.log('Mano vardas ir pavardė: ' + vardasPavarde);
 
 
 // Trecioji uzduotis
+
+
+let metai = 1982;
+let menuo = 1;
+let diena = 11;
+let gmmd = 'Gimimo data: ' + diena + '-' + menuo + '-' + metai;
+console.log(gmmd);
+let liekanaMetai = metai % 10;
+let liekanaMenuo = menuo % 10;
+let liekanaDiena = diena % 10;
+let ats = liekanaMetai + liekanaMenuo + liekanaDiena;
+console.log('Skaitmenų suma: ' + ats );
+
+
 const getLastNr = (nr) => parseInt((nr+'').slice(-1))
 
 const count = (metai, menuo, diena) => getLastNr(metai) + getLastNr(menuo) + getLastNr(diena)
